0615f7555ae294e20a0129a8db6e14feac7d04e2
rakesh kumar <rakesh@snakescript.com>
2024-09-24 18:58:02 +0530

    updated admin portal

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import User
-
-# Register your models here.
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'first_name', 'is_admin', 'is_staff')  # Fields to display in the list view
-    search_fields = ('email','is_admin', 'is_staff')
+from .forms import UserChangeForm, UserCreationForm
 
 
+
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    list_display = ('email', 'first_name', 'is_admin', 'is_staff')
+    list_filter = ["is_admin", "is_staff"]
+    
+    fieldsets = [
+        (None, {"fields": ["email", "password"]}),
+        ("Personal info", {"fields": ["first_name", "last_name"]}),
+        ("Permissions", {"fields": ["is_admin", "is_active", "is_staff", "is_superuser"]}),
+    ]
+     
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "first_name", "password1", "password2"],
+            },
+        ),
+    ]
+    search_fields = ["email", "first_name"]
+    ordering = ["email"]
+    filter_horizontal = []
+
+
+# register the new UserAdmin
 admin.site.register(User, UserAdmin)
+
+
+
+
+
--- base_app/admin.py
+++ base_app/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Website, WebsiteConfiguration
 
+
+
 class WebsiteAdmin(admin.ModelAdmin):
     list_display = ('get_website_name', 'url')  
-    search_fields = ('name',)        
+    search_fields = ('name', 'url')        
 
     def get_website_name(self, obj):
         if obj.name:
@@ -17,18 +19,18 @@ admin.site.register(Website, WebsiteAdmin)
 
 
 class WebsiteConfigurationAdmin(admin.ModelAdmin):
-    list_display = ('get_website_name', 'get_website_url', 'get_truncated_heading_text', 'color_scheme')  # Fields to display in the list view
-    search_fields = ('get_website_url', 'website')
+    list_display = ('get_website_name', 'get_website_url', 'get_truncated_heading_text', 'color_scheme')  
+    search_fields = ('website__name', 'website__url')
 
     def get_website_name(self, obj):
-        if obj.website.name:
+        if obj.website.name:      # Access the 'name' field from the related 'Website' model
             return obj.website.name[:25] + '...' if len(obj.website.name) > 25 else obj.website.name
 
     def get_website_url(self, obj):
         return obj.website.url  # Access the 'url' field from the related 'Website' model
     
     def get_truncated_heading_text(self, obj):
-        # Limit the number of characters to 30 (or any number you prefer)
+        # Limit the number of characters to 30
         if obj.heading_text:
             return obj.heading_text[:30] + '...' if len(obj.heading_text) > 30 else obj.heading_text
 
